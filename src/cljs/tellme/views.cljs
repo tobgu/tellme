f7b2f60c650b962e6f6be435fd1731cc2bddcc88
Tobias Gustafsson <tobias.l.gustafsson@gmail.com>
2015-03-21 09:02:25 +0100

    Initial signup form and some general form styling

--- src/cljs/tellme/views.cljs
+++ src/cljs/tellme/views.cljs
@@ -1,6 +1,7 @@
 (ns tellme.views
   (:require [re-frame.core :refer [subscribe dispatch]]
             [reagent-modals.modals :as reagent-modals]
+            [reagent.core :as reagent]
             [tellme.routes :as routes]))
 
 (defn input-value [input]
@@ -9,20 +10,77 @@
 (defn set-value! [target]
   (fn [source] (reset! target (input-value source))))
 
+; TODO: The notion of an error? true/false is probably to weak. Some more information should
+;       be possible to supply.
 (defn login-form []
   (let [user (atom nil)
         pass (atom nil)
         error? (subscribe [:error?])
         in-progress (subscribe [:in-progress])]
       (fn []
-        [:div
-        (if @error?
-          [:div (str "Invalid credentials, please try again")])
-          [:div.login-form
-              [:input {:on-change (set-value! user) :type "text" :placeholder "Username"}]
-              [:input {:on-change (set-value! pass) :type "password" :placeholder "Password"}]
-              [:span.button.login-button {:on-click #(dispatch [:login @user @pass])} "Login"]]])))
+        [:div.tellme-modal
+         [:h2 "Login"]
+         (if @error?
+           [:div.error-feedback (str "Invalid credentials, please try again")])
+         [:form.login-form
+          [:div.formgroup
+           [:input.tellme-input {:on-change (set-value! user) :type "text" :placeholder "Username"}]]
+          [:div.formgroup
+           [:input.tellme-input {:on-change (set-value! pass) :type "password" :placeholder "Password"}]]
+          [:div.btn.btn-lg.btn-primary {:on-click #(dispatch [:login @user @pass])} "Login"]]])))
 
+
+;; (defn input-valid?
+;;   "Valid if input is less than 10 characters"
+;;   [x]
+;;   (> 10 (count x)))
+
+;; (defn color [input]
+;;   (let [valid-color "green"
+;;         invalid-color "red"]
+;;     (if (input-valid? input)
+;;       valid-color invalid-color)))
+
+;; (defn home []
+;;   (let [state (reagent/atom {:user-input "some value"})]
+;;     (fn []
+;;       [:div [:h1 "Welcome to Reagent Cookbook!"]
+;;        [:span {:style {:padding "20px"
+;;                        :background-color (color (@state :user-input))}}
+;;         [:input {:value (@state :user-input)
+;;                  :on-change #(swap! state assoc :user-input (-> % .-target .-value))
+;;                  }]]])))
+
+
+(defn sign-up-form []
+  (let [account-info (reagent/atom {:user "", :password "", :first-name "", :last-name "", :e-mail ""
+                                    :org-name "", :org-short-name ""})
+        error? (subscribe [:error?])
+        in-progress (subscribe [:in-progress])]
+      (fn []
+        [:div.tellme-modal
+         [:h2 "Sign up"]
+         (if @error?
+           [:div.error-feedback (str "Invalid input, please try again")])
+         [:form.sign-up-form
+          [:div.formgroup
+           [:span (str (:org-short-name @account-info) "_")]
+           [:input.tellme-input {:on-change #(swap! account-info assoc :user (-> % .-target .-value)) :type "text" :placeholder "Username"}]]
+          [:div.formgroup
+           [:input.tellme-input {:on-change #(swap! account-info assoc :password (-> % .-target .-value)) :type "password" :placeholder "Password"}]]
+          [:div.formgroup
+           [:input.tellme-input {:on-change #(swap! account-info assoc :first-name (-> % .-target .-value)) :type "text" :placeholder "First name"}]]
+          [:div.formgroup
+           [:input.tellme-input {:on-change #(swap! account-info assoc :last-name (-> % .-target .-value)) :type "text" :placeholder "Last name"}]]
+          [:div.formgroup
+           [:input.tellme-input {:on-change #(swap! account-info assoc :e-mail (-> % .-target .-value)) :type "email" :placeholder "E-mail"}]]
+          [:div.formgroup
+           [:input.tellme-input {:on-change #(swap! account-info assoc :org-name (-> % .-target .-value)) :type "text" :placeholder "Organization name"}]]
+          [:div.formgroup
+           [:input.tellme-input {:on-change #(swap! account-info assoc :org-short-name (-> % .-target .-value)) :type "text" :placeholder "Organization short name"}]]
+          [:div.btn.btn-lg.btn-primary
+           {:on-click #(dispatch [:sign-up @account-info])} "Sign up"]]]))
+  )
 
 (defn start-page []
   [:div.container
@@ -31,7 +89,7 @@
     [:h1 "Welcome to TellMe"]
     [:p "Dedicated to simplifying your report workflow!"]
     [:div.btn.btn-lg.btn-primary {:on-click #(reagent-modals/modal! [login-form])} "Login"]
-    [:div.btn.btn-lg.btn-default {:on-click #(dispatch [:login-success])} "Sign up"]   ;; TODO Fix sign up
+    [:div.btn.btn-lg.btn-default {:on-click #(reagent-modals/modal! [sign-up-form])} "Sign up"]   ;; TODO Fix sign up
     ]])
 
 ;; This hack to hide the modal once on the start page works for now
